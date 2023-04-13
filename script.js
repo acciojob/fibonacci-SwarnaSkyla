766a15b94b6a58a92b68aba4b64b8ae321e8ab1f
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-04-13 11:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let n1=0,n2=1,n3;
 function fibonacci(num) {
 // your code here
+	for(let i=0;i<num;i++){
+		console.log(n1);
+		n3=n1+n2;
+		n1=n2;
+		n2=n3;
+	}
+	
 }
 
 module.exports = fibonacci;
